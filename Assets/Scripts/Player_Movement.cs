e9cff75f634d38b6088eca1e7047c55bb637b185
Dinh Luu <12748556@student.uts.edu.au>
2019-10-02 18:52:13 +1000

    Added player movement
    Added constant acceleration plus rotation for both players one and two.

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float movementSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //constant movement of the players vehicle.
+        movementSpeed += 0.1f * Time.deltaTime;
+        transform.position += transform.up * Time.deltaTime * movementSpeed;
+
+        //splits up the controls between player one and two.
+        if (gameObject.name.Equals("PlayerOneSpr"))
+        {
+            if (Input.GetKey(KeyCode.D))
+                RotateClockwise();
+            
+            if (Input.GetKey(KeyCode.A))
+                RotateCounterClockwise();
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+                RotateClockwise();
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+                RotateCounterClockwise();
+        }
+    }
+
+    private void RotateClockwise()
+    {
+        transform.Rotate(0, 0, -150.0f * Time.deltaTime);
+    }
+
+    private void RotateCounterClockwise()
+    {
+        transform.Rotate(0, 0, 150.0f * Time.deltaTime);
+    }
+}
