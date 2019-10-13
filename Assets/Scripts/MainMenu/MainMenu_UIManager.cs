2e08c9812028c1cdea6b1cc39ea880bbf9bd330e
Dinh Luu <12748556@student.uts.edu.au>
2019-10-13 21:49:06 +1100

    Completed main menu
    TODO: Make the main menu prettier

---
+++ Assets/Scripts/MainMenu/MainMenu_UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu_UIManager : MonoBehaviour
+{
+    //Quits the game
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    //The following methods will load the appropriate stage that the player chooses
+    public void LoadStageOne()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void LoadStageTwo()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+
+    public void LoadStageThree()
+    {
+        SceneManager.LoadScene(3, LoadSceneMode.Single);
+    }
+}
