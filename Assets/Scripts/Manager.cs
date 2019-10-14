f2ae9102e2640cdf34ce86290b4dc3c1abd229e8
Dinh Luu <12748556@student.uts.edu.au>
2019-10-14 14:41:20 +1100

    Laps added

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,16 +2,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
     public bool gameStart = false;
+    //NOTE: To get actual number of laps, devide totalLapCount by 2.
+    public int totalLapCount = 6;
+    GameObject player1, player2;
 
     void Start()
     {
         StartCoroutine(BeginCountdown());
+        player1 = GameObject.FindGameObjectWithTag("Player1");
+        player2 = GameObject.FindGameObjectWithTag("Player2");
     }
 
+    void Update()
+    {
+        //Checks if the players have reached the lap amount and determines who the winner is.
+        if (player1.GetComponent<Player_Manager>().currentLap == totalLapCount)
+        {
+            Debug.Log("PLAYER 1 WINS");
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+        }
+        if(player2.GetComponent<Player_Manager>().currentLap == totalLapCount)
+        {
+            Debug.Log("PLAYER 2 WINS");
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+        }
+    }
+
+    //Waiting for the race to start. Will wait for three seconds.
     IEnumerator BeginCountdown()
     {
         yield return new WaitForSeconds(3);
---
+++ Assets/Scripts/Player_Manager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Manager : MonoBehaviour
+{
+    public float movementSpeed = 1.0f;
+    //Note: One lap will count as 2 in backend, therefore totalLapCount / 2 will be the actual number of laps
+    public int currentLap = -2;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (GameObject.Find("Manager").GetComponent<Manager>().gameStart)
+        {
+            //constant movement of the players vehicle.
+            movementSpeed += 0.1f * Time.deltaTime;
+            transform.position += transform.up * Time.deltaTime * movementSpeed;
+
+            //splits up the controls between player one and two.
+            if (gameObject.name.Equals("PlayerOneSpr"))
+            {
+                if (Input.GetKey(KeyCode.D))
+                    RotateClockwise();
+
+                if (Input.GetKey(KeyCode.A))
+                    RotateCounterClockwise();
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.RightArrow))
+                    RotateClockwise();
+
+                if (Input.GetKey(KeyCode.LeftArrow))
+                    RotateCounterClockwise();
+            }
+        }
+    }
+
+    //Detects when the player hits a wall, and resets the player to a position just before they hit the wall.
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Wall" || col.gameObject.tag == "MovingWall")
+        {
+            movementSpeed = movementSpeed / 2.0f;
+            transform.position -= transform.up * 0.05f;
+        }
+        if (col.gameObject.tag == "Lap")
+        {
+            currentLap += 2;
+            Debug.Log(gameObject.name + currentLap);
+        }
+    }
+
+
+    //Rotates the player
+    void RotateClockwise()
+    {
+        transform.Rotate(0, 0, -150.0f * Time.deltaTime);
+    }
+
+    void RotateCounterClockwise()
+    {
+        transform.Rotate(0, 0, 150.0f * Time.deltaTime);
+    }
+    
+}
